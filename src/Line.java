2033b41aba706109400b854cb4f26e45d1308087
veggieboy <meanomenestcaesar@gmail.com>
2014-12-04 20:38:14 -0700

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -2,26 +2,32 @@ public class Line
 {
   private Point start;
   private Point end;
-  private Line() {
+  private Line() 
+  {
     start.setX(0);
     start.setY(0);
     end.setX(0);
     end.setY(0);
   }
-  public Line(Point startPoint, Point endPoint) {
+  public Line(Point startPoint, Point endPoint) 
+  {
     start=startPoint;
     end=endPoint;
   }
-  public double length() {
+  public double length() 
+  {
     return start.distance(end);
   }
-  public double slope() {
+  public double slope() 
+  {
     return (start.getY()-end.getY())/(start.getX()-end.getX());
   }
-  public String toString() {
+  public String toString() 
+  {
     return ("["+start.toString()+", "+end.toString()+"]");
   }
-  public void translate (int dx, int dy) {
+  public void translate (int dx, int dy) 
+  {
     start.translate(dx, dy);
     end.translate(dx, dy);
   }
