c2eed5353ad24747130819fe620fbd4f987f5325
veggieboy <meanomenestcaesar@gmail.com>
2014-12-04 20:35:25 -0700

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -1,38 +1,28 @@
 public class Line
 {
-  Point Start;
-  Point End;
-  
-  public Line ()
-  {
-    Start=new Point();
-    End=new Point ();
+  private Point start;
+  private Point end;
+  private Line() {
+    start.setX(0);
+    start.setY(0);
+    end.setX(0);
+    end.setY(0);
   }
-  
-  public Line (Point start, Point end)
-  {
-    start=new Point();
-    end=new Point();
+  public Line(Point startPoint, Point endPoint) {
+    start=startPoint;
+    end=endPoint;
   }
-  
-  public double distance()
-  {
-    return Math.sqrt((Start.getX()-End.getX())*(Start.getX()-End.getX())+(Start.getY()-End.getY())*(Start.getY()-End.getY()));
+  public double length() {
+    return start.distance(end);
   }
-  
-  public double slope()
-  {
-    return (End.getY()-Start.getY())/(End.getX()-Start.getX());
+  public double slope() {
+    return (start.getY()-end.getY())/(start.getX()-end.getX());
   }
-  
-  public String toString()
-  {
-    return "[("+Start.getX()+", "+End.getY()+")"+", "+"("+Start.getX()+", "+End.getY()+")]";
+  public String toString() {
+    return ("["+start.toString()+", "+end.toString()+"]");
   }
-  
-  public void translateLine(int dx, int dy)
-  {
-    Start.translate(dx, dy);
-    End.translate(dx, dy);
+  public void translate (int dx, int dy) {
+    start.translate(dx, dy);
+    end.translate(dx, dy);
   }
 }
